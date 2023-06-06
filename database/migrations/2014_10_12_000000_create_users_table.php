3220efba67d4d2b6a48682c76799d00e800527cb
Felipe Castilla <jfrodriguez6422@misena.edu.co>
2023-06-05 23:17:04 -0500

    Migracion con campos de usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('second_name');
+            $table->string('first_lastname');
+            $table->string('second_lastname');
+            $table->string('document_number');
+            $table->string('phone_number');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('password_verify');
             $table->rememberToken();
             $table->timestamps();
         });
