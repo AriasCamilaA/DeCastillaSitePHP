fd4b1051f2e6b90fc249eab8fb7f1bd277a72980
AriasCamilaA <caarias911@misena.edu.co>
2023-06-19 16:31:57 -0500

    Agregando modelos para poder utilizar la vistas

---
+++ app/Models/vw_pedidos_no_finalizados.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class vw_pedidos_no_finalizados extends Model
+{
+    use HasFactory;
+    protected $table = 'vw_pedidos_no_finalizados';
+    protected $primaryKey = 'ID_PEDIDO';
+
+    protected $fillable = [
+        'ID_PEDIDO',
+        'DESCRIPCION',
+        'FECHA',
+        'ESTADO',
+        'DOC_USUARIO',
+        'CELULAR',
+        'CLIENTE',
+    ];
+}
