3231dcf87987f8b47152bc9b41ce1512800524ea
AriasCamilaA <caarias911@misena.edu.co>
2023-06-22 00:08:01 -0500

    Estilos tab_table inventario

--- resources/views/inventario/visualizar.blade.php
+++ resources/views/inventario/visualizar.blade.php
@@ -4,15 +4,10 @@
     <link rel="stylesheet" href="{{asset("assets/css/tablas.css")}}">
     <link rel="stylesheet" href="{{asset("assets/css/botones.css")}}">
     <link rel="stylesheet" href="{{asset("assets/css/modalForms.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/tab_tabla.css")}}">
         <!-- ---------------------------------------------Contenido----------------------------------------- -->
         <div class="contenido">
             <h1>Inventario</h1>
-            <div class="opciones">
-                <input type="radio" name="rd_Estado" id="rd_btn-porAprobar" class="btn">
-                <label for="rd_btn-porAprobar" class="lbl_Estado btn">Productos</label>
-                <input type="radio" name="rd_Estado" id="rd_btn-aprobado" class="btn">
-                <label for="rd_btn-aprobado" class="lbl_Estado btn">Insumos</label>
-                </div>
             <div class="filtros">
                 <div>
                     <img src="{{asset("assets/icons/lupa.png")}}" alt="">
@@ -25,36 +20,52 @@
                 </div>
                 <img src="{{asset("assets/icons/excel.png")}}" alt="">
             </div>
-            <div class="tabla">
-                <table class="table table-hover table-striped">
-                    <thead>
-                      <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Insumo</th>
-                        <th scope="col">Entradas</th>
-                        <th scope="col">Salidas</th>
-                        <th scope="col">Stock</th>
-                        <th scope="col">Opciones</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        @foreach($vw_inventario_insumo as $insumos)
-                        <th>{{$insumos->ID}}</th>
-                        <td>{{$insumos->INSUMO}}</td>
-                        <td>{{$insumos->ENTRADAS}}</td>
-                        <td>{{$insumos->SALIDAS}}</td>
-                        <td>{{$insumos->STOCK}}</td>
-                        <td class="tabla__opcion">
-                            <a href="./verProducto">
-                               <img src="{{asset("assets/icons/visualizar.png")}}" alt="Visualizar">
-                           </a>
-                       </td>
-                      </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
-            </div>
+
+            <div class="tablaConTab">
+                <div>
+                  <button class="tablink bg-oscuro" onclick="openCity(event,'Insumos')">Insumos</button>
+                  <button class="tablink" onclick="openCity(event,'Productos')">Productos</button>
+                </div>
+                
+                <div id="Insumos" class="tab_content">
+                    <div class="tabla">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                              <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Insumo</th>
+                                <th scope="col">Entradas</th>
+                                <th scope="col">Salidas</th>
+                                <th scope="col">Stock</th>
+                                <th scope="col">Opciones</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                @foreach($vw_inventario_insumo as $insumos)
+                                <th>{{$insumos->ID}}</th>
+                                <td>{{$insumos->INSUMO}}</td>
+                                <td>{{$insumos->ENTRADAS}}</td>
+                                <td>{{$insumos->SALIDAS}}</td>
+                                <td>{{$insumos->STOCK}}</td>
+                                <td class="tabla__opcion">
+                                    <a href="./verProducto">
+                                       <img src="{{asset("assets/icons/visualizar.png")}}" alt="Visualizar">
+                                   </a>
+                               </td>
+                              </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                    </div>
+                </div>
+              
+                <div id="Productos" class="tab_content" style="display:none">
+{{-- ------------------------------------tabla productos --}}
+               </div>
+                </div>
+              </div>
+
         </div>
     </div>
     <!-- -------------------Modal de agregar------------------ -->
@@ -91,5 +102,5 @@
           </div>
         </div>
       </div>
-
+      <script src="{{asset('assets/js/tab_tabla.js')}}"></script>
 @endsection
