4a5d3c8da23fa56e89d0bba0e37cdaae302a9445
AriasCamilaA <caarias911@misena.edu.co>
2023-06-28 14:06:48 -0500

    Agregar errores login y busqueda de tablas

--- resources/views/pedidos/visualizarPedido.blade.php
+++ resources/views/pedidos/visualizarPedido.blade.php
@@ -138,93 +138,93 @@
     <script>
       function doSearch()
 
-{
+      {
 
-    const tableReg = document.getElementById('datos');
+          const tableReg = document.getElementById('datos');
 
-    const searchText = document.getElementById('searchTerm').value.toLowerCase();
+          const searchText = document.getElementById('searchTerm').value.toLowerCase();
 
-    let total = 0;
-
-
-
-    // Recorremos todas las filas con contenido de la tabla
-
-    for (let i = 1; i < tableReg.rows.length; i++) {
-
-        // Si el td tiene la clase "noSearch" no se busca en su cntenido
-
-        if (tableReg.rows[i].classList.contains("noSearch")) {
-
-            continue;
-
-        }
+          let total = 0;
 
 
 
-        let found = false;
+          // Recorremos todas las filas con contenido de la tabla
 
-        const cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
+          for (let i = 1; i < tableReg.rows.length; i++) {
 
-        // Recorremos todas las celdas
+              // Si el td tiene la clase "noSearch" no se busca en su cntenido
 
-        for (let j = 0; j < cellsOfRow.length && !found; j++) {
+              if (tableReg.rows[i].classList.contains("noSearch")) {
 
-            const compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+                  continue;
 
-            // Buscamos el texto en el contenido de la celda
-
-            if (searchText.length == 0 || compareWith.indexOf(searchText) > -1) {
-
-                found = true;
-
-                total++;
-
-            }
-
-        }
-
-        if (found) {
-
-            tableReg.rows[i].style.display = '';
-
-        } else {
-
-            // si no ha encontrado ninguna coincidencia, esconde la
-
-            // fila de la tabla
-
-            tableReg.rows[i].style.display = 'none';
-
-        }
-
-    }
+              }
 
 
 
-    // mostramos las coincidencias
+              let found = false;
 
-    const lastTR=tableReg.rows[tableReg.rows.length-1];
+              const cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
 
-    const td=lastTR.querySelector("td");
+              // Recorremos todas las celdas
 
-    lastTR.classList.remove("hide", "red");
+              for (let j = 0; j < cellsOfRow.length && !found; j++) {
 
-    if (searchText == "") {
+                  const compareWith = cellsOfRow[j].innerHTML.toLowerCase();
 
-        lastTR.classList.add("hide");
+                  // Buscamos el texto en el contenido de la celda
 
-    } else if (total) {
+                  if (searchText.length == 0 || compareWith.indexOf(searchText) > -1) {
 
-        td.innerHTML="Se ha encontrado "+total+" coincidencia"+((total>1)?"s":"");
+                      found = true;
 
-    } else {
+                      total++;
 
-        lastTR.classList.add("red");
+                  }
 
-        td.innerHTML="No se han encontrado coincidencias";
+              }
 
-    }
+              if (found) {
 
-}
+                  tableReg.rows[i].style.display = '';
+
+              } else {
+
+                  // si no ha encontrado ninguna coincidencia, esconde la
+
+                  // fila de la tabla
+
+                  tableReg.rows[i].style.display = 'none';
+
+              }
+
+          }
+
+
+
+          // mostramos las coincidencias
+
+          const lastTR=tableReg.rows[tableReg.rows.length-1];
+
+          const td=lastTR.querySelector("td");
+
+          lastTR.classList.remove("hide", "red");
+
+          if (searchText == "") {
+
+              lastTR.classList.add("hide");
+
+          } else if (total) {
+
+              td.innerHTML="Se ha encontrado "+total+" coincidencia"+((total>1)?"s":"");
+
+          } else {
+
+              lastTR.classList.add("red");
+
+              td.innerHTML="No se han encontrado coincidencias";
+
+          }
+
+      }
     </script>
